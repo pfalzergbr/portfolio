b6cc01ab60541f1c04cc18ce816ef16059873e51
Gabor Pfalzer <pfalzer@gmail.com>
2021-02-18 13:37:00 +0000

    add layout style, base grid

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,5 +1,6 @@
 import Footer from "../components/layoutElements/Footer"
 import Navigation from "../components/layoutElements/Navigation"
+import styles from "./styles/layout.module.scss";
 
 export interface LayoutProps {
   children
@@ -7,7 +8,7 @@ export interface LayoutProps {
  
 const Layout: React.FC<LayoutProps> = ({children}) => {
   return ( 
-  <div className="App">
+  <div className={styles.App}>
     <Navigation/>
     {children}
     <Footer />
