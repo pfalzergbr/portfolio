ae309b2fad671db6be815433c046b3e6417fa488
Gabor Pfalzer <pfalzer@gmail.com>
2021-02-18 16:10:00 +0000

    style navbar and add transition

--- components/UI/NavLink.tsx
+++ components/UI/NavLink.tsx
@@ -1,9 +1,20 @@
-export interface NavLinkProps {
-  
-}
- 
-const NavLink: React.FC<NavLinkProps> = () => {
-  return ( <div></div> );
-}
- 
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import styles from './styles/NavLink.module.scss';
+
+
+export interface NavLinkProps { href: string; text: string}
+
+const NavLink: React.FC<NavLinkProps> = ({ href, text }) => {
+  const router = useRouter();
+
+  const className = `${styles.navlink} ${router.pathname === href ? styles.active: ''}`
+
+  return (
+    <Link href={href}>
+      <a className={className}>{text}</a>
+    </Link>
+  );
+};
+
 export default NavLink;
