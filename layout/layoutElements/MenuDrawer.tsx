291a26427f46938c0e838b04e733965a1fbd933e
Gabor Pfalzer <pfalzer@gmail.com>
2021-02-21 15:13:48 +0000

    add hamburger menu

---
+++ layout/layoutElements/MenuDrawer.tsx
@@ -0,0 +1,9 @@
+export interface MenuDrawerProps {
+  
+}
+ 
+const MenuDrawer: React.FC<MenuDrawerProps> = () => {
+  return ( <div></div> );
+}
+ 
+export default MenuDrawer;
--- layout/layoutElements/Navigation.tsx
+++ layout/layoutElements/Navigation.tsx
@@ -1,19 +1,33 @@
+import { useState } from 'react';
+import Menu from './Menu';
 import NavLink from './NavLink';
 import styles from './styles/Navigation.module.scss';
 
 //Todo - add hamburger menu
 
 const Navigation: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
-    <nav>
-      <div className={styles.navlinkContainer}>
-        <NavLink href="/" text="Home" />
-        <NavLink href="/projects" text="Projects" />
-        <NavLink href="/skills" text="Skills" />
-        <NavLink href="/contact" text="Contact" />
-        <NavLink href="/about" text="About" />
-      </div>
-    </nav>
+    <>
+      {/* <div className=""></div> */}
+      <nav className={styles.navigation}>
+        <div className={styles.menuContainer}>
+          <Menu isMenuOpen={isMenuOpen} toggleOpen={toggleOpen} />
+        </div>
+        <div className={styles.navlinkContainer}>
+          <NavLink href='/' text='Home' />
+          <NavLink href='/projects' text='Projects' />
+          <NavLink href='/skills' text='Skills' />
+          <NavLink href='/contact' text='Contact' />
+          <NavLink href='/about' text='About' />
+        </div>
+      </nav>
+    </>
   );
 };
 
