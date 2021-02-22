cdc22a08a6e6a1cf9915741d21514247af3cd6f4
Gabor Pfalzer <pfalzer@gmail.com>
2021-02-22 09:24:43 +0000

    tidy up hero scss module

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import AnimatedRoute from '../../layout/AnimatedRoute';
 import indexStyles from './index.module.scss';
 import styles from '../../components/about/styles/Section.module.scss';
