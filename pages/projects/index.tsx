dc9f612ae0830cb6e7a859d310391c096996e591
Gabor Pfalzer <pfalzer@gmail.com>
2021-02-23 09:21:09 +0000

    add project page structure

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -3,6 +3,7 @@ import AnimatedRoute from '../../layout/AnimatedRoute';
 // import UnderConstruction from '../../layout/layoutElements/UnderConstruction';
 import { projects } from '../../data/projects';
 import { Project } from '../../components/projects/ProjectItem';
+import styles from './index.module.scss';
 
 export interface Props {
   projects: Project[];
@@ -10,12 +11,17 @@ export interface Props {
 
 const Portfolio: React.FC<Props> = ({ projects }) => {
   return (
-    <AnimatedRoute className=''>
+    <AnimatedRoute className={styles.projects}>
       {/* <UnderConstruction /> */}
       <ProjectGrid
         projects={projects}
         header='Main Projects'
-        description='Full Stack Applications and mor complicated, longer projects'
+        description='Full Stack Applications and more complex, longer projects I tend to spend more time with.'
+      />
+      <ProjectGrid
+        projects={projects}
+        header='Learning & Practice'
+        description='I really enjoy learning new technologies. These are short, fun projects I build specifically for picking up, or practice new technologies.'
       />
     </AnimatedRoute>
   );
