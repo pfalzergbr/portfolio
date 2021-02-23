34d88622bc4c13de326199b4475c307528ebfb3a
Gabor Pfalzer <pfalzer@gmail.com>
2021-02-23 08:59:05 +0000

    connect an style projects

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,14 +1,32 @@
+import ProjectGrid from '../../components/projects/ProjectGrid';
 import AnimatedRoute from '../../layout/AnimatedRoute';
-import UnderConstruction from '../../layout/layoutElements/UnderConstruction';
+// import UnderConstruction from '../../layout/layoutElements/UnderConstruction';
+import { projects } from '../../data/projects';
+import { Project } from '../../components/projects/ProjectItem';
 
-export interface Props {}
+export interface Props {
+  projects: Project[];
+}
 
-const Portfolio: React.FC<Props> = () => {
+const Portfolio: React.FC<Props> = ({ projects }) => {
   return (
-    <AnimatedRoute className="">
-      <UnderConstruction />
+    <AnimatedRoute className=''>
+      {/* <UnderConstruction /> */}
+      <ProjectGrid
+        projects={projects}
+        header='Main Projects'
+        description='Full Stack Applications and mor complicated, longer projects'
+      />
     </AnimatedRoute>
   );
+};
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      projects,
+    },
+  };
 };
 
 export default Portfolio;
