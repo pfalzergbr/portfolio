cd13e3c0114fe5c12e403be5e50f58a435c0bc86
Gabor Pfalzer <pfalzer@gmail.com>
2021-02-24 09:20:59 +0000

    add overlay modal controller to TechStack

---
+++ components/skills/SkillModal.tsx
@@ -0,0 +1,9 @@
+export interface SkillModalProps {
+  
+}
+ 
+const SkillModal: React.FC <SkillModalProps> = () => {
+  return ( <div></div> );
+}
+ 
+export default SkillModal;
--- components/skills/TechStack.tsx
+++ components/skills/TechStack.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import TechStackBlock from './TechStackBlock';
 import styles from './styles/TechStack.module.scss';
 import SkillHeader from './SkillHeader';
@@ -14,19 +15,27 @@ export interface TechStackProps {
 
 const TechStack: React.FC<TechStackProps> = ({ techStack }) => {
   const { languagesBlock, frontendBlock, backendBlock, otherBlock } = techStack;
+  const [isModalOpen, setIsModalOpen ] = useState(false);
+  
+  const toggleOpen = () => {
+    setIsModalOpen(!isModalOpen);
+  }
+
   return (
-    <div className={styles.techStackContainer}>
-      <SkillHeader
-        title='Skills & Stack'
-        subtitle=''
-      />
-      <div className={styles.techStackGrid}>
-        <TechStackBlock techList={languagesBlock} />
-        <TechStackBlock techList={frontendBlock} />
-        <TechStackBlock techList={backendBlock} />
-        <TechStackBlock techList={otherBlock} />
+    <>
+      {isModalOpen && (
+        <div className={styles.overlay} onClick={toggleOpen} key='menu'></div>
+      )}
+      <div className={styles.techStackContainer}>
+        <SkillHeader title='Skills & Stack' subtitle='' />
+        <div className={styles.techStackGrid}>
+          <TechStackBlock techList={languagesBlock} />
+          <TechStackBlock techList={frontendBlock} />
+          <TechStackBlock techList={backendBlock} />
+          <TechStackBlock techList={otherBlock} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
