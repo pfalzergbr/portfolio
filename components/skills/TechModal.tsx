ba13ffaf619ef85d19f30f63521af5f59ae02d3c
Gabor Pfalzer <pfalzer@gmail.com>
2021-02-25 09:08:58 +0000

    connect techData

--- components/skills/TechModal.tsx
+++ components/skills/TechModal.tsx
@@ -1,77 +1,57 @@
 import Link from 'next/link';
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
 import styles from './styles/TechModal.module.scss';
 import TechIcon from './TechIcon';
 import { Tech } from '../../data/techData';
-import {modalTransition } from '../../transitions/modalTransition';
+import { modalTransition } from '../../transitions/modalTransition';
 //Add connection
-
-
 
 export interface TechModalProps {
   techData: Tech;
 }
 
-const TechModal: React.FC<TechModalProps> = ({techData}) => {
-  // const {icon, label, description, projects} = techData;
+const TechModal: React.FC<TechModalProps> = ({ techData }) => {
+  const { icon, label, description, projects } = techData;
+
 
   return (
-    <motion.div 
-    initial='initial'
+    <motion.div
+      initial='initial'
       animate='enter'
       exit='exit'
       variants={modalTransition}
-    
-    className={styles.techModal}>
+      className={styles.techModal}
+    >
       <div className={styles.techModalContent}>
         <div className={styles.techModalHeader}>
-          <TechIcon icon='typescript' />
-          <h2 className={styles.techModalTitle}>TypeScript</h2>
+          <TechIcon icon={icon} />
+          <h2 className={styles.techModalTitle}>{label}</h2>
         </div>
-        <p className={styles.techModalDescription}>
-          Superset of JavaScript, making it typesafe, and way more scalable.
-          Since I switched from vanilla, It is definitely my preferred way to
-          write JavaScript code.
-        </p>
+        <p className={styles.techModalDescription}>{description}</p>
       </div>
-      <div className={styles.techModalProjects}>
-        <h3 className={styles.techModalProjectsHeader}>
-          Check it out in projects:
-        </h3>
-        <ul className={styles.techModalProjectsList}>
-          <Link href='#'>
-            <li className={styles.techModalProject}>
-              <h4 className={styles.techModalProjectTitle}>Haiku Project</h4>
-              <p className={styles.techModalProjectDescription}>
-                Recommends you haiku poems based on season and mood. A nice way
-                for modern poets to share their work.
-              </p>
-            </li>
-          </Link>
-          <Link href='#'>
-            <li className={styles.techModalProject}>
-              <h4 className={styles.techModalProjectTitle}>
-                Rock Paper Scissors
-              </h4>
-              <p className={styles.techModalProjectDescription}>
-                Junior Dev classic rebuilt for practice, utilizing reusable
-                TypeScript, and ships with a (free) Day-1 DLC: Lizard-Spock.
-                Thanks, Dr Cooper.
-              </p>
-            </li>
-          </Link>
-          <Link href='#'>
-            <li className={styles.techModalProject}>
-              <h4 className={styles.techModalProjectTitle}>Bookish</h4>
-              <p className={styles.techModalProjectDescription}>
-                Buying books is cool. Reading them is even cooler. This
-                application helps you schedule your reading, working through
-                your shelf.
-              </p>
-            </li>
-          </Link>
-        </ul>
-      </div>
+      {projects && (
+        <div className={styles.techModalProjects}>
+          <h3 className={styles.techModalProjectsHeader}>
+            Check it out in projects:
+          </h3>
+          <ul className={styles.techModalProjectsList}>
+            {projects.map((project) => {
+              return (
+                <Link key={project.title} href={project.path}>
+                  <li className={styles.techModalProject}>
+                    <h4 className={styles.techModalProjectTitle}>
+                      {project.title}
+                    </h4>
+                    <p className={styles.techModalProjectDescription}>
+                      {project.snippet}
+                    </p>
+                  </li>
+                </Link>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </motion.div>
   );
 };
