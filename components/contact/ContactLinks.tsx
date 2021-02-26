c1808f906dd845f58bc284a9b160734afecb8535
Gabor Pfalzer <pfalzer@gmail.com>
2021-02-26 08:53:21 +0000

    finish contact section

--- components/contact/ContactLinks.tsx
+++ components/contact/ContactLinks.tsx
@@ -1,21 +1,32 @@
-import SocialIcon from '../landing/SocialIcon';
 import styles from './styles/Contacts.module.scss';
 
 const ContactLinks: React.FC = () => {
   return (
     <div className={styles.contactLinks}>
-      <div className={styles.contactContainer}>
-        <SocialIcon logo='linkedin' url='' />
-        <p className={styles.contactText}>Connect on LinkedIn</p>
-      </div>
-      <div className={styles.contactContainer}>
-        <SocialIcon logo='twitter' url='' />
-        <p className={styles.contactText}>Follow me on Twitter</p>
-      </div>
-      <div className={styles.contactContainer}>
-        <SocialIcon logo='github' url='' />
-        <p className={styles.contactText}>Check out my Github</p>
-      </div>
+      <a className={styles.contactLink} href='https://www.linkedin.com/in/gabor-peter-pfalzer-10552224/'>
+        <div className={styles.contactContainer}>
+          <svg className={styles.socialIcon} focusable={true}>
+            <use href={`/social/sprite.svg#social-linkedin`}></use>
+          </svg>
+          <p className={styles.contactText}>Connect on LinkedIn</p>
+        </div>
+      </a>
+      <a className={styles.contactLink} href='https://twitter.com/GaborPfalzer'>
+        <div className={styles.contactContainer}>
+          <svg className={styles.socialIcon} focusable={true}>
+            <use href={`/social/sprite.svg#social-twitter`}></use>
+          </svg>
+          <p className={styles.contactText}>Follow me on Twitter</p>
+        </div>
+      </a>
+      <a className={styles.contactLink} href='https://github.com/pfalzergbr'>
+        <div className={styles.contactContainer}>
+          <svg className={styles.socialIcon} focusable={true}>
+            <use href={`/social/sprite.svg#social-github`}></use>
+          </svg>
+          <p className={styles.contactText}>Check out my Github</p>
+        </div>
+      </a>
     </div>
   );
 };
