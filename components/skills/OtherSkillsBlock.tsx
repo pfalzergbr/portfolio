86bf0773cf3d7d357f25a8cc35a5e10acad67db3
Gabor Pfalzer <pfalzer@gmail.com>
2021-03-03 12:18:53 +0000

    add fade-in effect to other skills

--- components/skills/OtherSkillsBlock.tsx
+++ components/skills/OtherSkillsBlock.tsx
@@ -1,5 +1,5 @@
 import { motion, AnimatePresence } from 'framer-motion';
-import { fadeInGroup } from '../../transitions/fadeInGroup';
+import { fadeInGroup, fadeInItem } from '../../transitions/fadeInGroup';
 import SkillHeader from './SkillHeader';
 import styles from './styles/OtherSkillsBlock.module.scss';
 
@@ -20,14 +20,14 @@ const OtherSkillsBlock: React.FC<OtherSkillsBlockProps> = () => {
           animate='show'
           className={styles.skillsList}
         >
-          <li className={styles.skill}>Curious, always looking to get better and learn new skills</li>
-          <li className={styles.skill}>Fast and efficient learner, experienced teacher</li>
-          <li className={styles.skill}>Disciplined and extremely patient.</li>
-          <li className={styles.skill}>Excellent communication and people skills (ex-bartender)</li>
-          <li className={styles.skill}>Organized and methodical, great teamworker</li>
-          <li className={styles.skill}>Highly analytical, but focused on reality.</li>
-          <li className={styles.skill}>Management and Leadership experience, also good follower</li>
-          <li className={styles.skill}>Good business and financial awareness</li>
+          <motion.li variants={fadeInItem} className={styles.skill}>Curious, always looking to get better and learn new skills</motion.li>
+          <motion.li variants={fadeInItem} className={styles.skill}>Fast and efficient learner, experienced teacher</motion.li>
+          <motion.li variants={fadeInItem} className={styles.skill}>Disciplined and extremely patient.</motion.li>
+          <motion.li variants={fadeInItem} className={styles.skill}>Excellent communication and people skills (ex-bartender)</motion.li>
+          <motion.li variants={fadeInItem} className={styles.skill}>Organized and methodical, great teamworker</motion.li>
+          <motion.li variants={fadeInItem} className={styles.skill}>Highly analytical, but focused on reality.</motion.li>
+          <motion.li variants={fadeInItem} className={styles.skill}>Management and Leadership experience, also good follower</motion.li>
+          <motion.li variants={fadeInItem} className={styles.skill}>Good business and financial awareness</motion.li>
         </motion.ul>
       </AnimatePresence>
     </div>
