f44b50caa7b003b01ae7735a7705c5e12c3f7804
Gabor Pfalzer <pfalzer@gmail.com>
2021-04-11 17:00:52 +0100

    add head to index.ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,24 +11,14 @@ function MyApp({ Component, pageProps, router }: AppProps) {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta charSet='utf-8' />
         <title>Gabor Pfalzer - Portfolio</title>
-        <meta
-          name='description'
-          content='Full Stack Web Development Portfolio, London'
-        />
-        <meta
-          name='title'
-          property='og:title'
-          content='Gabor Pfalzer - Portfolio'
-        />
+        <meta property='og:title' content='Gabor Pfalzer - Portfolio' />
         <meta property='type' content='Websote' />
         <meta
-          name='image'
           property='og:image'
           content='https://res.cloudinary.com/pfalzer-dev/image/upload/v1618154691/portfolio-min_v4oztd.png'
         />
         <meta
-          name='og:description'
-          property='og:description'
+          property='og:descion'
           content='Full Stack Web Development Portfolio, London'
         />
         <meta name='author' content='Gabor Pfalzer' />
