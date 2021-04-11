0b83c4c248a5a12d91e4d27b4dba1f4c7c22c297
Gabor Pfalzer <pfalzer@gmail.com>
2021-04-11 16:44:22 +0100

    add head to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,45 @@
 import '../styles/globals.scss';
 import { AnimatePresence } from 'framer-motion';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import Layout from '../layout/Layout';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
-    <Layout>
-      <AnimatePresence exitBeforeEnter>
-        {/* <Component {...pageProps} key={router.route}/> */}
-        <Component {...pageProps} />
-      </AnimatePresence>
-    </Layout>
+    <>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta charSet='utf-8' />
+        <title>Gabor Pfalzer - Portfolio</title>
+        <meta
+          name='description'
+          content='Full Stack Web Development Portfolio, London'
+        />
+        <meta
+          name='title'
+          property='og:title'
+          content='Gabor Pfalzer - Portfolio'
+        />
+        <meta property='type' content='Websote' />
+        <meta
+          name='image'
+          property='og:image'
+          content='https://res.cloudinary.com/pfalzer-dev/image/upload/v1618154691/portfolio-min_v4oztd.png'
+        />
+        <meta
+          name='og:description'
+          property='og:description'
+          content='Full Stack Web Development Portfolio, London'
+        />
+        <meta name='author' content='Gabor Pfalzer' />
+      </Head>
+      <Layout>
+        <AnimatePresence exitBeforeEnter>
+          {/* <Component {...pageProps} key={router.route}/> */}
+          <Component {...pageProps} />
+        </AnimatePresence>
+      </Layout>
+    </>
   );
 }
 
